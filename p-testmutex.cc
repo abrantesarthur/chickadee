04a5d2188228eccd95367a40b44b4c3873e6f305
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-05-05 05:06:06 -0400

    pset5: done with code

--- p-testmutex.cc
+++ p-testmutex.cc
@@ -3,29 +3,39 @@
 extern uint8_t end[];
 
 void process_main() {
+
     // create a futex-based lock
     mutex m;
 
-    // grab it
-    m.lock();
-
-    // fork child
+    // child grabs the lock first
     pid_t p = sys_fork();
     if(p == 0) {
-
-        // trylock fails
-        assert_eq(m.trylock(), 0);
-
-        sys_msleep(50);
-
         assert_eq(m.trylock(), 1);
+        sys_msleep(20);
+        m.unlock();
+        sys_exit(0);
+    }
+    sys_msleep(20);
+    assert_eq(m.trylock(), 0);
+    sys_msleep(50);
+    assert_eq(m.trylock(), 1);
+    pid_t ch = sys_waitpid(p);
+    assert_eq(ch, p);
 
 
+    // test with lock
+    m.unlock();
+    m.lock();
+
+    p = sys_fork();
+    if(p == 0) {
+        assert_eq(m.trylock(), 0);
+        sys_msleep(50);
+        assert_eq(m.trylock(), 1);
         sys_exit(0);
     }
 
     sys_msleep(20);
-
     m.unlock();
 
     console_printf("testmutex succeeded.\n");
